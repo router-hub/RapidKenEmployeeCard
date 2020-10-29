612d2a6c3cd475d2b10f3079a1e1e60d4d9062f7
Router <adityamanitiwari5@gmail.com>
2020-10-29 15:32:37 +0530

    Some bugs fixed and making fully responsive

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -7,16 +7,20 @@ import { Router } from '@angular/router';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
- public link = "/registration";
+//  public link = "/registration";
   constructor(public router: Router) { }
 
   ngOnInit(): void {
+
+    // this.link = this.router.getCurrentNavigation;
+    // console.log(this.router.getCurrentNavigation());
+    // this.link = (this.router.url).toString();
   }
 
-  Link(link){
-    this.link = link;
-    console.log(this.link);
-    this.router.navigate([link]);
-  }
+  // Link(link){
+  //   this.link = link;
+  //   console.log(this.link);
+  //   this.router.navigate([link]);
+  // }
 
 }
